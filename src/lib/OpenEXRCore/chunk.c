c88b1058302bff457cd7a646a99847dc2ce2ebdc
Nick Porcino <meshula@hotmail.com>
2023-05-11 11:06:03 -0700

    Simplify the ifdef logic around atomics

--- src/lib/OpenEXRCore/chunk.c
+++ src/lib/OpenEXRCore/chunk.c
@@ -13,27 +13,10 @@
 #include <limits.h>
 #include <string.h>
 
-/**************************************/
 
-/* for testing, we include a bunch of internal stuff into the unit tests which are in c++ */
-/* see internal_structs.h for details on the msvc guard. */
-#if !defined(_MSC_VER)
-#    if defined __has_include
-#        if __has_include(<stdatomic.h>)
-#            define EXR_HAS_STD_ATOMICS 1
-#        endif
-#    endif
-#endif
-
-#ifdef EXR_HAS_STD_ATOMICS
-#    include <stdatomic.h>
-#elif defined(_MSC_VER)
-
+#if defined(_MSC_VER) && not defined(__cplusplus)
 /* msvc w/ c11 support is only very new, until we know what the preprocessor checks are, provide defaults */
 #    include <windows.h>
-
-#    define atomic_load(object) InterlockedOr64 ((int64_t volatile*) object, 0)
-
 static inline int
 atomic_compare_exchange_strong (
     uint64_t volatile* object, uint64_t* expected, uint64_t desired)
@@ -44,9 +27,6 @@ atomic_compare_exchange_strong (
     *expected = prev;
     return 0;
 }
-
-#else
-#    error OS unimplemented support for atomics
 #endif
 
 /**************************************/
